5453673700642b0720436ecbb0fa8bddb8a385f0
Diego Luiz <diegoluizcsti@gmail.com>
2019-12-19 17:09:36 -0300

    rotas privadas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-
+import React from 'react';
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <div />;
+  return <h1>Dashboard</h1>;
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,22 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch } from 'react-router-dom';
 
-import SignIn from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
+import Route from './Route';
 
-import Dashboard from "../pages/Dashboard";
-import Profile from "../pages/Profile";
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact compenent={SignIn} />
-      <Route path="/register" compenent={SignUp} />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" compenent={Dashboard} />
-      <Route path="/profile" compenent={Profile} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
     </Switch>
   );
 }
