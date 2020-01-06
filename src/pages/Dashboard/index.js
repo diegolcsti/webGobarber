4d96559dd09875cf80ccc8d060daac93d92a59d8
Diego Luiz <diegoluizcsti@gmail.com>
2020-01-06 15:37:22 -0300

    Adicionando a data de hoje e botões de navegações de datas no dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 // import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDays() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDays() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
         <button type="button">
-          <MdChevronLeft size={36} color="#fff" />
+          <MdChevronLeft size={36} color="#fff" onClick={handlePrevDays} />
         </button>
-        <strong>31 de Maio</strong>
+        <strong>{dateFormatted}</strong>
         <button type="button">
-          <MdChevronRight size={36} color="#fff" />
+          <MdChevronRight size={36} color="#fff" onClick={handleNextDays} />
         </button>
       </header>
 
