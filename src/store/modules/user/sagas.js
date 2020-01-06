f1db0f4349f81a10c1539ce804d07b15a07849f9
Diego Luiz <diegoluizcsti@gmail.com>
2020-01-06 10:05:06 -0300

    avatar de usuário no Update

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -8,9 +8,14 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 export function* updateProfile({ payload }) {
   try {
     console.tron.log('updateProfile');
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
 
-    const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
 
     const response = yield call(api.put, 'users', profile);
 
